e8add5731e068082260bff25ae50b3e29c84cc1c
Guillermo Vera <vfguille@gmail.com>
2017-04-18 11:15:02 +0200

    Trabajando conexión

---
+++ CatalogoDVD/DaoDVDMySQL.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using MySql.Data.MySqlClient;
+
+namespace CatalogoDVD
+{
+    public class DaoDVDMySQL
+    {
+        // Encapsula con la BD
+        public MySqlConnection conexion;
+
+        public Conectar(string srv, string db, string user, string pwd)
+        {
+            // server=localhost;
+            // database=catalogo;
+            // uid=usr_catalogo
+            // pwd=12345678;
+            string cadenaConexion = "server=" + srv + 
+        }
+    }
+}
--- CatalogoDVD/Program.cs
+++ CatalogoDVD/Program.cs
@@ -4,13 +4,16 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
 namespace CatalogoDVD
 {
     class Program
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Aplicación de gestión de DVDs.");
 
+            Console.ReadLine();
         }
     }
 }
