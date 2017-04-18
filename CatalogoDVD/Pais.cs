2502868c58a776393a59bdb9ec88cd8550bee35e
Guillermo Vera <vfguille@gmail.com>
2017-04-18 10:51:51 +0200

    Commit inicial

---
+++ CatalogoDVD/Pais.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CatalogoDVD
+{
+    public class Pais
+    {
+        private string _nombre;
+        private string iso2;
+
+        public string Nombre
+        {
+            get { return _nombre; }
+            set { _nombre = value; }
+        }
+        
+        public string Iso2
+        {
+            get { return iso2; }
+            set { iso2 = value; }
+        }
+    }
+}
