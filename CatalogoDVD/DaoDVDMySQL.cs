cfcc307d7bebae39a5b673a7a1292d36bb93c4ed
Guillermo Vera <vfguille@gmail.com>
2017-04-25 10:58:12 +0200

    Añadido método Desconectar, catálogo de DVDs y PA.

--- CatalogoDVD/DaoDVDMySQL.cs
+++ CatalogoDVD/DaoDVDMySQL.cs
@@ -13,12 +13,7 @@ namespace CatalogoDVD
 
         public bool Conectar(string srv, string db, string user, string pwd)
         {
-            // server=localhost;
-            // database=catalogo;
-            // uid=usr_catalogo
-            // pwd=12345678;
             string cadenaConexion = "server=" + srv + ";" + "database=" + db + ";" + "uid=" + user + ";" + "pwd=" + pwd + ";";
-
 
             try
             {
@@ -36,5 +31,70 @@ namespace CatalogoDVD
                 }
             }
         }
+
+        public void Desconectar()
+        {
+            try
+            {
+                conexion.Close();
+            }
+            catch(MySqlException)
+            {
+                throw;  // Captura una posible excepción de MySQL y la relanza.
+            }
+        }
+
+        public bool Conectado()
+        {
+            if (conexion != null)
+            {
+                return conexion.State == System.Data.ConnectionState.Open;
+            } else
+            {
+                return false;
+            }
+        }
+
+        public List<Dvd> SeleccionarPA(string codigo)
+        {
+            List<Dvd> resultado = new List<Dvd>();
+            int resul;
+
+            MySqlCommand cmd = new MySqlCommand("selectDVD", conexion); // Instrucciones MySQL
+            cmd.CommandType = System.Data.CommandType.StoredProcedure;
+
+            // Armado de los parámetros del procedimiento almacenado
+            cmd.Parameters.AddWithValue("@codi", codigo); // Método para añadir un parámetro al comando, aconsejado por MariaDB.
+            cmd.Parameters.AddWithValue("@titu", null);
+            cmd.Parameters.AddWithValue("@arti", null);
+            cmd.Parameters.AddWithValue("@elPais", null);
+            cmd.Parameters.AddWithValue("@comp", null);
+            cmd.Parameters.AddWithValue("@prec", null);
+            cmd.Parameters.AddWithValue("@elAnio", null);
+
+            cmd.Parameters.AddWithValue("@resul", MySqlDbType.Int16);
+            cmd.Parameters["@resul"].Direction = System.Data.ParameterDirection.Output;
+
+            MySqlDataReader lector = cmd.ExecuteReader();
+            resul = (int)cmd.Parameters["@resul"].Value;    // Por definir qué hacer en caso de resul != 0.
+
+            while (lector.Read())
+            {
+                Dvd undvd = new Dvd();
+                undvd.Codigo = int.Parse(lector["codigo"].ToString());
+                undvd.Titulo = lector["titulo"].ToString();
+                undvd.Artista = lector["artista"].ToString();
+                undvd.Pais = lector["pais"].ToString();
+                undvd.Compania = lector["compania"].ToString();
+                undvd.Precio = double.Parse(lector["precio"].ToString());
+                undvd.Anio = lector["anio"].ToString();
+
+                resultado.Add(undvd);
+            }
+
+            lector.Close();
+            return resultado;
+        }
+
     }
 }
--- CatalogoDVD/UI.cs
+++ CatalogoDVD/UI.cs
@@ -13,12 +13,17 @@ namespace CatalogoDVD
         static string usr = "usr_catalogo";
         static string pwd = "12345678";
 
+        static List<Dvd> listado;
+
         public UI()
         {
             dao = new DaoDVDMySQL();
             PedirOpcion();
         }
 
+        /// <summary>
+        /// Opciones de menú Principal.
+        /// </summary>
         static void Menu()
         {
             // Única clase que puede hablar con la consola, para no crear dependecia.
@@ -27,12 +32,34 @@ namespace CatalogoDVD
             Console.WriteLine(" ===================================");
             Console.WriteLine();
             Console.WriteLine(" (0) Conectar con la Base de Datos");
+            Console.WriteLine(" (1) Desconectar con la Base de Datos");
+            Console.WriteLine(" (2) Listar DVD por código [PA]");
             Console.WriteLine(" (Q) Fin del programa");
             Console.WriteLine();
             Console.WriteLine();
             Console.Write(" Opción: ");
         }
+        /// <summary>
+        /// Recorrido de la llamada a un select de los registros solicitados.
+        /// </summary>
+        static void MostrarListado()
+        {
+            if (dao.Conectado())
+            {
+                foreach (Dvd unDvd in listado)
+                {
+                    unDvd.ToString();
+                }
+            } else
+            {
+                Console.WriteLine("No hay una conexión válida.");
+            }
+            
+        }
 
+        /// <summary>
+        /// Solicitud de opción por consola.
+        /// </summary>
         static void PedirOpcion()
         {
             ConsoleKeyInfo opcion;
@@ -46,17 +73,44 @@ namespace CatalogoDVD
                     switch ((char)opcion.Key)
                     {
                         case '0': // Conexión BD.
-                            if (dao.Conectar(host, bd, usr, pwd))                             
+                            if (!dao.Conectado())
                             {
-                                Console.WriteLine();
-                                Console.WriteLine();
-                                Console.WriteLine("***********************************************************");
-                                Console.WriteLine(" Conexión exitosa a la Base de Datos: " + bd);
-                                Console.WriteLine("***********************************************************");
-                                Console.WriteLine();
+                                if (dao.Conectar(host, bd, usr, pwd))                               
+                                    Console.WriteLine();
+                                    Console.WriteLine();
+                                    Console.WriteLine("***********************************************************");
+                                    Console.WriteLine(" Conexión exitosa a la Base de Datos: " + bd);
+                                    Console.WriteLine("***********************************************************");
+                                    Console.WriteLine();
+                            } else
+                            {
+                                Console.WriteLine("No hay una conexión establecida.");
                             }
+                                break;                            
+                           
+                        case '1': // Desconexión BD.                           
+                                Console.WriteLine();
+                                if (dao.Conectado())
+                                {
+                                    dao.Desconectar();
+                                    Console.WriteLine();
+                                    Console.WriteLine("***********************************************************");
+                                    Console.WriteLine(" Desconexión exitosa a la Base de Datos: " + bd);
+                                    Console.WriteLine("***********************************************************");
+                                    Console.WriteLine(); 
+                                } else
+                                {
+                                    Console.WriteLine("No hay una conexión establecida.");
+                                }
+                                                         
                             break;
-                        default:
+
+                        case '2':   // Selección de DVD a través de Procedimientos Almacenados.
+                            Console.WriteLine();
+                            listado = new List<Dvd>();
+                            listado = dao.SeleccionarPA(null);
+                            MostrarListado();
+                            Console.WriteLine();
                             break;
                     }
                 }
